8f8f997fae3a4245c74cede0b00ab15486c2478c
gzovkic <g.zovkic@web.de>
2026-01-28 18:12:11 +0100

    gin

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,9 +6,7 @@
 
 int main()
 {
-    // Seed the randomizer for the Robotomy form
     std::srand(std::time(0));
-
     try 
 	{
         Bureaucrat boss("The Boss", 1);
@@ -17,30 +15,25 @@ int main()
         std::cout << "--- Shrubbery Test ---" << std::endl;
         ShrubberyCreationForm shrub("home");
         
-        // 1. Try to execute before signing
         boss.executeForm(shrub); 
         
-        // 2. Sign and execute
         boss.signForm(shrub);
-        boss.executeForm(shrub); // Should create home_shrubbery file
+        boss.executeForm(shrub);
 
         std::cout << "\n--- Robotomy Test ---" << std::endl;
         RobotomyRequestForm robo("Bender");
         boss.signForm(robo);
         boss.executeForm(robo);
-        boss.executeForm(robo); // Run twice to see the 50/50 chance
+        boss.executeForm(robo);
 
         std::cout << "\n--- Presidential Test ---" << std::endl;
         PresidentialPardonForm pardon("Arthur Dent");
         
-        // 3. Intern tries to sign (too low grade)
         intern.signForm(pardon);
         
-        // 4. Boss signs, but Intern tries to execute (too low grade)
         boss.signForm(pardon);
         intern.executeForm(pardon);
         
-        // 5. Boss does everything
         boss.executeForm(pardon);
 
     }
